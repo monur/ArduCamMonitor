30014df086c2d1b68da3a199760acacff5af6be1
monur <monur@nuvaapps>
2013-08-31 20:46:59 +0300

    -Working now

---
+++ PCVideo.ino
@@ -0,0 +1,75 @@
+#define PIN_SCE   7
+#define PIN_RESET 6
+#define PIN_DC    5
+#define PIN_SDIN  4
+#define PIN_SCLK  3
+
+#define LCD_C     LOW
+#define LCD_D     HIGH
+
+#define LCD_X     84
+#define LCD_Y     6
+
+#define bufferSize  4
+byte frameStart[] = {0x80, 0x01, 0x80, 0x01};
+byte buffer[bufferSize];
+
+void setup(){
+  LcdInitialise();
+  delay(500);
+  Serial.begin(115200);
+}
+
+void loop(){
+   while(Serial.available() > 0){
+     byte b = Serial.read();
+     
+     for(int i = 0; i < bufferSize-1; i++){
+       buffer[i] = buffer[i+1];
+     }
+     buffer[bufferSize-1] = b;
+     boolean isFrameStart = true;
+     for(int i = 0; i < bufferSize; i++){
+         if(buffer[i] != frameStart[i])
+           isFrameStart = false;
+     }
+     
+     if(isFrameStart){
+        LcdWrite(LCD_C, 0x80 ); //select X Address 0 of the LCD Ram
+        LcdWrite(LCD_C, 0x40 ); //select Y Address 0 of the LCD Ram - Reset is not working for some reason, so I had to set these addresses
+     }else{
+         //for(int i = 0; i < bufferSize; i++){
+             LcdWrite(LCD_D, b);
+         //}
+     }
+   }
+}
+
+void LcdInitialise(void){
+  pinMode(PIN_SCE, OUTPUT);
+  pinMode(PIN_RESET, OUTPUT);
+  pinMode(PIN_DC, OUTPUT);
+  pinMode(PIN_SDIN, OUTPUT);
+  pinMode(PIN_SCLK, OUTPUT);
+  delay(200);
+  digitalWrite(PIN_RESET, LOW);
+  delay(500);
+  digitalWrite(PIN_RESET, HIGH);
+  LcdWrite(LCD_C, 0x21 );  // LCD Extended Commands.
+  LcdWrite(LCD_C, 0xB1 );  // Set LCD Vop (Contrast). 
+  LcdWrite(LCD_C, 0x04 );  // Set Temp coefficent. //0x04
+  LcdWrite(LCD_C, 0x14 );  // LCD bias mode 1:48. //0x13
+  LcdWrite(LCD_C, 0x0C );  // LCD in normal mode.
+  LcdWrite(LCD_C, 0x20 );
+  LcdWrite(LCD_C, 0x80 ); //select X Address 0 of the LCD Ram
+  LcdWrite(LCD_C, 0x40 ); //select Y Address 0 of the LCD Ram - Reset is not working for some reason, so I had to set these addresses
+  LcdWrite(LCD_C, 0x0C );
+}
+
+void LcdWrite(byte dc, byte data){
+  digitalWrite(PIN_DC, dc);
+  digitalWrite(PIN_SCE, LOW);
+  shiftOut(PIN_SDIN, PIN_SCLK, MSBFIRST, data);
+  digitalWrite(PIN_SCE, HIGH);
+}
+
---
+++ PCVideo/PCVideo.pde
@@ -0,0 +1,69 @@
+import processing.video.*;
+import processing.serial.*;
+
+final int lcdWidth = 84;
+final int lcdHeight = 48;
+
+Capture video;
+Serial myPort;
+int frameStart[] = {0x80, 0x01, 0x80, 0x01};
+int[] lcdPixels = new int[lcdWidth * lcdHeight];
+
+public void setup() {
+  size(lcdWidth, lcdHeight, P2D);
+  video = new Capture(this, lcdWidth, lcdHeight, 15);
+  println(Serial.list());
+  myPort = new Serial(this, Serial.list()[0], 115200);
+  loadPixels();
+}
+
+public void captureEvent(Capture c) {
+  c.read();
+}
+
+void draw() {
+  background(0);
+  int pixelWidth = width / video.width;
+  int pixelHeight = height / video.height;
+
+  int index = 0;
+  for (int y = 0; y < video.height; y++) {
+    float luminance = 0.0;
+    for (int x = 0; x < video.width; x++) {
+      int pixelColor = video.pixels[index];
+      int r = (pixelColor >> 16) & 0xff;
+      int g = (pixelColor >> 8) & 0xff;
+      int b = pixelColor & 0xff;
+
+      luminance = 0.3*r + 0.59*g + 0.11*b;
+      
+      pixels[index] = video.pixels[index];
+      if(luminance > 80)
+        lcdPixels[index] = 0xffffffff;
+      else
+        lcdPixels[index] = 0x00000000;
+      
+      index++;
+    }
+  }
+  updatePixels();
+  
+  //send to Serial port
+  
+  for(int i = 0; i< frameStart.length; i++)
+      myPort.write((byte)frameStart[i]);
+
+  for(int y = 0; y < lcdHeight / 8; y++){
+     for(int x = 0; x < lcdWidth; x++){
+        int by = 0x00;
+        for(int a = 0; a < 8; a++){
+           by = by >> 1;
+           if(lcdPixels[(y*8+a)*lcdWidth+x] == 0x00000000){
+              by = by | 0x80;
+           } 
+        }
+        myPort.write((byte)by);
+     } 
+  }
+  delay(50);
+}
